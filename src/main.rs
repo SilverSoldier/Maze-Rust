e1d412f9aab0e3b4cf4515774b924c3bc1e1e82e
Kavya <gkavya10@gmail.com>
2017-03-16 12:52:16 +0530

    @ now moves across the screen

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,32 @@ const SCREEN_WIDTH: i32 = 80;
 // set frames per second limit
 const LIMIT_FPS: i32 = 20; 
 
+/* Function to handle keyboard input
+ * Input: root of type tcod::console::Root mutable since we need to redraw @, player_x and
+ * player_y of type i32 mutable since we need to modify it as the arrow keys are pressed.
+ * Output: bool: true -> exit game, false -> continue
+ */
+fn keyboard_input(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool{
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+    let key = root.wait_for_keypress(true); //turn based otherwise real time
+    match key {
+        // code tells whether up or down etc.
+        // .. => ignore other fields
+        Key {code: Escape, ..} => return true,
+
+        Key {code: Up, ..} => *player_y -= 1,
+        Key {code: Down, ..} => *player_y += 1,
+        Key {code: Left, ..} => *player_x -= 1,
+        Key {code: Right, ..} => *player_x += 1,
+        // Escape exits game
+
+        _ => {}
+
+    };
+    return false;
+}
+
 fn main() {
     let mut root: Root = Root::initializer()
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -18,12 +44,22 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH/2;
+    let mut player_y = SCREEN_HEIGHT/2;
+
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+
         // BackgroundFlag::None => ignore default background color
-        
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         root.flush(); // draws everything on the screen at once
-        root.wait_for_keypress(true);
+
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+
+        let end_game = keyboard_input(&mut root, &mut player_x, &mut player_y);
+        if end_game {
+            break;
+        }
+
     }
 }
