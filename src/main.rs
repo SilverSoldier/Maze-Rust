9cf899b900ed406478a21e576a93bcdc7ecf5d27
Kavya <gkavya10@gmail.com>
2017-03-22 19:39:47 +0530

    created object player and added functionality to it

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,64 @@ extern crate tcod;
 
 use tcod::console::*;
 use tcod::colors;
+use tcod::colors::Color;
 
 const SCREEN_HEIGHT: i32 = 50;
 const SCREEN_WIDTH: i32 = 80;
 // set frames per second limit
 const LIMIT_FPS: i32 = 20; 
 
+struct Object{
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object{
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x : x,
+            y: y,
+            char : char,
+            color : color,
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    pub fn erase(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
 /* Function to handle keyboard input
  * Input: root of type tcod::console::Root mutable since we need to redraw @, player_x and
  * player_y of type i32 mutable since we need to modify it as the arrow keys are pressed.
  * Output: bool: true -> exit game, false -> continue
  */
-fn keyboard_input(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool{
+fn keyboard_input(root: &mut Root, player: &mut Object) -> bool{
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     let key = root.wait_for_keypress(true); //turn based otherwise real time
     match key {
         // code tells whether up or down etc.
         // .. => ignore other fields
+        // Escape exits game
         Key {code: Escape, ..} => return true,
 
-        Key {code: Up, ..} => *player_y -= 1,
-        Key {code: Down, ..} => *player_y += 1,
-        Key {code: Left, ..} => *player_x -= 1,
-        Key {code: Right, ..} => *player_x += 1,
-        // Escape exits game
+        Key {code: Up, ..} => player.move_by(0, -1),
+        Key {code: Down, ..} => player.move_by(0, 1),
+        Key {code: Left, ..} => player.move_by(-1, 0),
+        Key {code: Right, ..} => player.move_by(1, 0),
 
         _ => {}
 
@@ -42,21 +75,23 @@ fn main() {
         .font_type(FontType::Greyscale)
         .init();
 
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH/2;
-    let mut player_y = SCREEN_HEIGHT/2;
+    let mut player = Object::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', colors::WHITE);
 
     while !root.window_closed() {
-        root.set_default_foreground(colors::WHITE);
+        // con.set_default_foreground(colors::WHITE);
 
         // BackgroundFlag::None => ignore default background color
-        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        player.draw(&mut con);
+
+        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
         root.flush(); // draws everything on the screen at once
 
-        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        player.erase(&mut con);
 
-        let end_game = keyboard_input(&mut root, &mut player_x, &mut player_y);
+        let end_game = keyboard_input(&mut root, &mut player);
         if end_game {
             break;
         }
